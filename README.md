c68377f80a627ccfcd2f780a9eef5b40f642c171
Sorawit <sorawit.pixel@gmail.com>
2021-03-19 01:34:00 +0700

    Checkpoint for Debug Zebra Priner

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # zpl-printer-backend
+
+## How to run
+
+```bash
+node app.js
+```
+
+## Installation
+
+### Dependency
+
+```bash
+"node": v15.11.0
+"npm": 7.6.0
+"yarn": 1.22.5
+```
+
+Note: If you have problem about `Printer Package`
+
+```
+npm install printer --target_arch=ia32
+npm install printer --target_arch=x64
+```
+
+more detail - https://www.npmjs.com/package/printer
--- app.js
+++ app.js
@@ -6,30 +6,27 @@ var fakeAPI = require("./module/fakeAPI.json");
 var isOdd = true; // Check Array of fakeAPI
 const printerName = "ZDesigner ZD220-203dpi ZPL";
 
-// Check Array of fakeAPI
-if (fakeAPI.data.length % 2 == 0) {
-  console.log("--->templateDUAL");
-  isOdd = false;
-}
-
-// Check Printer Zebra Name
-util.CheckPrinterName();
-
-// Command Print on ZPL Format
-util.MapItemsToPrintStickerZPL(printerName, isOdd, fakeAPI.data);
-
 const app = express();
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.text({ type: "*/*" }));
 
-app.post("/handle", (req, res) => {
-  console.log("req.body", req.body);
-  res.status(201).json(req.body);
+app.post("/zpl_print", (req, res) => {
+  // Check Array of fakeAPI
+  if (fakeAPI.data.length % 2 == 0) {
+    //console.log("--->templateDUAL");
+    isOdd = false;
+  }
+
+  // Check Printer Zebra Name
+  util.CheckPrinterName();
+
+  // Command Print on ZPL Format
+  util.MapItemsToPrintStickerZPL(printerName, isOdd, fakeAPI.data);
+
+  res.status(201).json({ status: "success" });
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hi ZPL Printer Backend");
 });
 
 app.listen(3000, () => {
