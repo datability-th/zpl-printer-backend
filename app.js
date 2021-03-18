c312cdd53b5f760620096914b007366614f41954
Sorawit <sorawit.pixel@gmail.com>
2021-03-19 01:15:17 +0700

    Checkpoint Clear Lib

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const util = require("./module/utilZPLPrinter");
 var fakeAPI = require("./module/fakeAPI.json");
 var isOdd = true; // Check Array of fakeAPI
@@ -14,3 +17,21 @@ util.CheckPrinterName();
 
 // Command Print on ZPL Format
 util.MapItemsToPrintStickerZPL(printerName, isOdd, fakeAPI.data);
+
+const app = express();
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text({ type: "*/*" }));
+
+app.post("/handle", (req, res) => {
+  console.log("req.body", req.body);
+  res.status(201).json(req.body);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Start server at port 3000.");
+});
